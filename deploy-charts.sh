8479ad9907676dfe500816cf06e1c120c1200ca9
kPsarakis <kpsarakis94@gmail.com>
2021-09-10 18:16:46 +0200

    [ci skip] set the version of the minio helm chart

--- deploy-charts.sh
+++ deploy-charts.sh
@@ -6,7 +6,7 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 
 helm repo update
 
-helm install -f helm-config/minio-helm-values.yaml minio bitnami/minio
+helm install --version v7.3.2 -f helm-config/minio-helm-values.yaml minio bitnami/minio
 helm install -f helm-config/redis-helm-values.yaml redis bitnami/redis
 helm install -f helm-config/rabbitmq-helm-values.yaml rabbitmq bitnami/rabbitmq
 helm install -f helm-config/nginx-helm-values.yaml nginx ingress-nginx/ingress-nginx
