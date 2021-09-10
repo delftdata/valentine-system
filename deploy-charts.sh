07624fed956b89cf6c8be4d982b9b553624e1ade
kPsarakis <kpsarakis94@gmail.com>
2021-09-10 19:25:45 +0200

    [ci skip] set the version of the helm charts

--- deploy-charts.sh
+++ deploy-charts.sh
@@ -7,7 +7,7 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
 helm install --version v7.3.0 -f helm-config/minio-helm-values.yaml minio bitnami/minio
-helm install -f helm-config/redis-helm-values.yaml redis bitnami/redis
-helm install -f helm-config/rabbitmq-helm-values.yaml rabbitmq bitnami/rabbitmq
-helm install -f helm-config/nginx-helm-values.yaml nginx ingress-nginx/ingress-nginx
-helm install -f helm-config/postgres-helm-values.yaml postgres cetic/postgresql
+helm install --version v15.3.2 -f helm-config-minikube/redis-helm-values.yaml redis bitnami/redis
+helm install --version v8.22.0 -f helm-config-minikube/rabbitmq-helm-values.yaml rabbitmq bitnami/rabbitmq
+helm install --version v4.0.1 -f helm-config/nginx-helm-values.yaml nginx ingress-nginx/ingress-nginx
+helm install --version v0.2.1 -f helm-config-minikube/postgres-helm-values.yaml postgres cetic/postgresql
